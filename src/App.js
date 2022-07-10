8e92c6998318316a216113a68b328c81430c35f2
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-07-10 14:53:49 -0500

    Added Clarifai API to app and it can now read images that are submitted through url. ALso displays the submitted image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-  {/*<FaceRecognition />*/}
-    </div>
+const apiKey = process.env.REACT_APP_API_KEY
+const app = new Clarifai.App({apiKey: apiKey});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input:event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then
+      (function (response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    }, 
+    function (err) {
+      
+    }
   );
 }
-
+  render() {
+    return (
+      <div className="App">
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange={
+            this.onInputChange
+          }
+          onButtonSubmit={
+            this.onButtonSubmit
+          }/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
+}
 export default App;
