15e9b5a1e182e69cb5ab0e7f584be57ccd221362
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-07-13 19:12:57 -0500

    Created signin page for app and made face recognition work

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
+//import Register from './components/Register/Register';
 import './App.css';
 
 const apiKey = process.env.REACT_APP_API_KEY
@@ -15,43 +17,63 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
+      route: 'signin'
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
-    this.setState({input:event.target.value});
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
+    this.setState({imageUrl: this.state.input});
     app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL,
+    .predict
+    (Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-      .then
-      (function (response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    }, 
-    function (err) {
-      
-    }
-  );
-}
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = () => {
+    this.setState({route: 'home'});
+  }
   render() {
     return (
       <div className="App">
         <Navigation/>
-        <Logo/>
-        <Rank/>
-        <ImageLinkForm onInputChange={
-            this.onInputChange
-          }
-          onButtonSubmit={
-            this.onButtonSubmit
-          }/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
-      </div>
+        { this.state.route === 'signin' 
+          ? <Signin onRouteChange = {this.onRouteChange} />
+          : <div>
+              <Logo/>
+              <Rank/>
+              <ImageLinkForm 
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+            />
+          <FaceRecognition box={this.state.box} imageUrl= {this.state.imageUrl} />
+          </div>
+        }
+        </div> 
     );
   }
 }
